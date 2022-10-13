590db2740237baa1a3ba21d128a358b75c457895
Quasar32 <academytown@gmail.com>
2022-10-13 16:58:13 -0500

    Basic tile editor complete

---
+++ src/menu.hpp
@@ -0,0 +1,12 @@
+#ifndef MENU_HPP
+#define MENU_HPP
+
+#define ID_MENU 0x0100 
+#define ID_ACCELERATOR 0x0101 
+
+#define IDM_NEW 0x0200 
+#define IDM_OPEN 0x0201 
+#define IDM_SAVE 0x0202 
+#define IDM_SAVE_AS 0x0203
+
+#endif
